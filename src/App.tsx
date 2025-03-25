30f854c1016ccea5cfb3055b626bacb1f97c3028
Leandro Batista <leandrobouwierphp@gmail.com>
2025-03-24 21:09:12 -0300

    terminando main

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import styles from "./app.module.css"
 
 import { Header } from "./components/Header"
+import { Tip } from "./components/Tip"
+import { Letter } from "./components/Letter"
 
-function handleRestartGame(){
+function handleRestartGame() {
   alert("Reiniciar o jogo")
 }
 
@@ -10,7 +12,15 @@ export default function App() {
   return (
     <div className={styles.container}>
       <main>
-        <Header  current={5} max={10} onRestart={handleRestartGame}/>
+        <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Tip tip="Uma das Linguagens de programacao mais utilizadas" />
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
       </main>
     </div>
   )
--- src/components/Tip/index.tsx
+++ src/components/Tip/index.tsx
@@ -1,8 +1,19 @@
 
+import styles from "./styles.module.css"
+import tipIcon from "../../assets/tip.svg"
+
 type Props = {
     tip: string
 }
 
-export function Tip({ tip }: Props){
-    
+export function Tip({ tip }: Props) {
+    return (
+        <div className={styles.tip}>
+            <img src={tipIcon} alt="icone dica" />
+            <div>
+                <h3>Dica</h3>
+                <p>{tip}</p>
+            </div>
+        </div>
+    )
 }
